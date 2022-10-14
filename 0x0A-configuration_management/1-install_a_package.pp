cb8fde1e3fa01671a34b0e3101f24c74d0e70872
abenetsol <solabenet@gmail.com>
2022-10-14 09:23:08 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,20 @@
-# installs the package puppet-lint
+# Install a package using Puppet
+
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
+}
+
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
+}
 
 package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
 }
